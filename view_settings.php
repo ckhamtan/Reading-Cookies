4d790592d0956739b25055cf36f62e79b319ffa7
ckhamtan <60579464+ckhamtan@users.noreply.github.com>
2020-03-29 12:15:57 -0700

    Add files via upload

---
+++ view_settings.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset-"utf-8">
+<title>View Your Settings</title>
+<style type="text/css">
+body {
+<?php // Script 9.2 - view_settings.php
+
+// Check for a fond_size value:
+if (isset($_COOKIE['font_size'])) {
+print "\t\tfont-size:" .
+htmlentities($_COOKIE['font_size']) . ";\n";
+} else {
+print "\t\tfont-size: meduim;";
+}
+
+// Check for a font_color value:
+if (isset($_COOKIE['font_color'])) {
+print "\t\tcolor: #" .
+htmlentities($_COOKIE['font_color']) . ";\n";
+} else {
+print "\t\tcolor: #000;";
+}
+?>
+}
+</style>
+</head>
+
+<body>
+<p><a href="customize.php">Customize Your Settings</a></p>
+<p><a href="reset.php">Reset your Settings</a></p>
+
+<p>Yadda yadda yadda yadda yadda yadda yadda
+yadda yadda yadda yadda yadda yadda
+yadda yadda yadda </p>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
